4ab5d6ce9c98281cf2a49777d8cce736a9b8f8c9
Yan <yanc.amaral@gmail.com>
2022-06-11 14:32:55 -0300

    versao04

--- script.js
+++ script.js
@@ -1,10 +1,43 @@
+let prato; 
+let bebida; 
+let sobremesa; 
 
-function selecionar(elemento) {
+function selecionarPrato(elemento) {
+    prato = elemento.innerHTML;
     let pratoSelecionado = document.querySelector(".selecionado");
     if(pratoSelecionado !== null){
         pratoSelecionado.classList.remove("selecionado");
     }
     elemento.classList.add("selecionado");
+    mudaBotao();
     
 }
 
+function selecionarBebida(elemento) {
+    bebida = elemento.innerHTML;
+    let bebidaSelecionada = document.querySelector(".bebidas .selecionado");
+    if(bebidaSelecionada !== null){
+        bebidaSelecionada.classList.remove("selecionado");
+    }
+    elemento.classList.add("selecionado");
+    mudaBotao();
+}
+
+function selecionarSobremesa(elemento) {
+    sobremesa = elemento.innerHTML;
+    let sobremesaSelecionada = document.querySelector(".sobremesas .selecionado");
+    if(sobremesaSelecionada !== null){
+        sobremesaSelecionada.classList.remove("selecionado");
+    }
+    elemento.classList.add("selecionado");
+    mudaBotao();
+}
+
+
+function mudaBotao(){
+    const botao = document.querySelector("button");
+    if(prato && bebida && sobremesa){
+       botao.style.backgroundColor = "#32b72f";
+       botao.innerHTML = "Fechar pedido";
+    }
+}
